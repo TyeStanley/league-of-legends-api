5902e7aad792bd39501dd7fe64b2264b353ea558
Tye Stanley <tyestanley090@gmail.com>
2022-11-29 20:01:55 -0800

    added league profileicons and updated hardcoded summoner page

--- client/src/pages/SummonerPage/summonerpage.js
+++ client/src/pages/SummonerPage/summonerpage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './summonerpage.css';
-import dummyImg from '../../assets/homepage-background-image.jpg';
+import placeHolder from '../../assets/img/profileicon/508.png';
 
 export default function Summonerpage(props) {
   return (
@@ -8,12 +8,14 @@ export default function Summonerpage(props) {
       <section className="summonerpage__icon-and-name-wrapper">
         <div className="summonerpage__img-and-level-wrapper">
           <div className="summonerpage__img">
-            <img src={dummyImg} alt="dummyImg" />
+            <img src={placeHolder} alt="placeholder" />
           </div>
           <div className="summonerpage__level">528</div>
         </div>
-        <h3 className="summonerpage__name">Summoner</h3>
-        <button className="btn summonerpage__update-btn">Update</button>
+        <div className="summonerpage__name-and-btn-wrapper">
+          <h3 className="summonerpage__name">Summoner</h3>
+          <button className="btn summonerpage__update-btn">Update</button>
+        </div>
       </section>
     </div>
   )
