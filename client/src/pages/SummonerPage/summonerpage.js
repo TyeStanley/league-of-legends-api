f0596e52a324ef0b670c26982f8fe0b0655efd00
Tye Stanley <tyestanley090@gmail.com>
2022-12-01 18:51:07 -0800

    added functionality to summoner page to search for accounts

--- client/src/pages/SummonerPage/summonerpage.js
+++ client/src/pages/SummonerPage/summonerpage.js
@@ -4,17 +4,50 @@ import './summonerpage.css';
 //import placeHolder from '../../assets/img/profileicon/508.png';
 
 export default function Summonerpage(props) {
-  const [account, setAccount] = useState('not set yet');
+  const [account, setAccount] = useState({
+    profileIconId: 508,
+    name: 'Nameless',
+    summonerLevel: 0
+  });
+  const [currentInitInfo, setInitInfo] = useState({ region: '', name: '' });
+
   const profileIconLink = `http://ddragon.leagueoflegends.com/cdn/12.22.1/img/profileicon/${account.profileIconId}.png`;
 
+  // useEffect for fetching using props from homepage input information (1 time use)
   useEffect(() => {
-    if (props.length !== 0) {
-      fetch(`http://localhost:3001/api/region/${props.region}/name/${props.summonerName}`)
-        .then(response => response.json())
-        .then(data => { setAccount(data) })
-        .catch(error => { console.error('Error fetching data: ', error) })
-    }
+    if (props.length !== 0) apiInitialCall(props.region, props.summonerName);
   }, []);
+
+  // Gathers the input for name and region when on the summoner page
+  useEffect(() => {
+    apiInitialCall(currentInitInfo.region, currentInitInfo.name);
+  }, [currentInitInfo])
+
+  // Uses the enter button to submit user input and region to fetch data from riot api
+  function handleInputSubmit(e) {
+    e.preventDefault();
+
+    // get summoner name input
+    let currentSummonerName = document.querySelector('.summonerpage__search-input');
+    
+    // assign the text value from select to variable called region
+    const selection = document.querySelector('.summonerpage__region-select');
+    const currentRegion = selection.options[selection.selectedIndex].value;
+    
+    // sets initial info for name and region input collections
+    setInitInfo({ region: currentRegion, name: currentSummonerName.value });
+    
+    // empty the value in the input after submitting
+    currentSummonerName.value = '';
+  } 
+
+  // Reusable function to make the initial api call for league account
+  function apiInitialCall(region, name) {
+    fetch(`http://localhost:3001/api/region/${region}/name/${name}`)
+      .then(response => response.json())
+      .then(data => { setAccount(data) })
+      .catch(error => { console.error('Error fetching data: ', error) })
+  }
 
   return (
     <div className="summonerpage">
@@ -39,13 +72,14 @@ export default function Summonerpage(props) {
           <span className="summonerpage__search-span">Search</span>
           <input className="input summonerpage__search-input" type="text" />
         </div>
-        <button className="btn summonerpage__search-btn" >Enter</button>
+        <button className="btn summonerpage__search-btn" onClick={handleInputSubmit}>Enter</button>
       </section>
 
       <section className="summonerpage__icon-and-name-wrapper">
         <div className="summonerpage__img-and-level-wrapper">
           <div className="summonerpage__img">
-            <img src={profileIconLink} alt="Placeholder" />
+            <img src={profileIconLink} alt="League of Legends" />
+            
           </div>
           <div className="summonerpage__level">{account.summonerLevel}</div>
         </div>
