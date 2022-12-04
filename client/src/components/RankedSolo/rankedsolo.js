202788655ab8bf94af727c1f44f63cd0958ef2c6
Tye Stanley <tyestanley090@gmail.com>
2022-12-03 19:57:07 -0800

    added html hardcoded ranked solo stats component

--- client/src/components/RankedSolo/rankedsolo.js
+++ client/src/components/RankedSolo/rankedsolo.js
@@ -4,7 +4,20 @@ import './rankedsolo.css';
 export default function Rankedsolo() {
   return (
     <section className="summonerpage__ranked-solo">
-
+      <h3>Ranked Solo</h3>
+      <div>
+        <div>
+          <img />
+        </div>
+        <div>
+          <div>Platinum 4</div>
+          <div>1 LP</div>
+        </div>
+        <div>
+          <div>12W 3L</div>
+          <div>Win Rate 80%</div>
+        </div>
+      </div>
     </section>
   )
 }
